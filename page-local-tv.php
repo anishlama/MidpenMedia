bd594c677bc60cb616ec6172dce5d6b8f24c3d6d
Anish <anish@nangsal.com>
2017-03-22 17:51:17 +0545

    recode: new channel changes and code optimization

--- page-local-tv.php
+++ page-local-tv.php
@@ -82,167 +82,75 @@ var isMobile = {
                     <div id="video-con"><div class="tabbable tabs-right">
 
 							<ul class="tab-content">
-								<li id="pane1" class="tab-pane active">
-									<div class="video-con" id='my-video26'></div>
-									<script type='text/javascript'>
-									if( isMobile.Android() ) {
-										jQuery('#my-video26').html('<a href="rtsp://ss1.midpenmedia.org:1935/live-channels/ch26_2700kbps">Arts, Issues &amp; Entertainment</a>');
-									} else {
-										jwplayer('my-video26').setup({
-											playlist: [{
-												sources : [
-													{ file: 'http://ss1.midpenmedia.org:1935/live-channels/ngrp:ch26_max2700kbps/jwplayer.smil' },
-													{ file: 'http://ss1.midpenmedia.org:1935/live-channels/ngrp:ch26_max2700kbps/playlist.m3u8' }
-												],
-												// title: "Channel 26",
-												image: '<?php echo get_template_directory_uri(); ?>/images/tv-img.png'
-											 }],
-											primary: "flash",
-											width: '505',
-											height: '265'
-										});
+								
+								<?php
+								
+								$pane_id	=	1;
+								
+								$channel_list	=	array(
+													array("id"	=>	26, "server_id"	=>	1, "title" =>	"Government etc" ),
+													array("id"	=>	27, "server_id"	=>	2, "title" =>	"Bay Voice TV Regional Programming", 'url'	=> 'http://reflect.channel27.creatv.cablecast.tv/live/live.m3u8' ),
+													array("id"	=>	28, "server_id"	=>	2, "title" =>	"Youth, Education and Sports" ),
+													array("id"	=>	29, "server_id"	=>	1, "title" =>	"Government etc" ),
+													array("id"	=>	30, "server_id"	=>	2, "title" =>	"Arts, Issues and Lifestyles" ),
+													array("id"	=>	75, "server_id"	=>	2, "title" =>	"Diversity and Culture" ),
+												);
+								$output_string	=	'';
+								foreach($channel_list as $ch):
+								
+									$ch_id	=	$ch["id"];
+									$ch_title	=	$ch["title"];
+									$server_id	=	$ch["server_id"];
+									
+									$is_active	=	$pane_id	==	1 ? ' active ' : '';
+									
+									
+									ob_start();
+									print_now_and_next($ch_id);
+									$now_and_next = ob_get_contents();
+									ob_end_clean();
+									
+									$rtsp_url	=	"rtsp://ss" . $server_id . ".midpenmedia.org:1935/live-chans/ch" . $ch_id . "_all";
+									$stream_url	=	'{ file: "http://ss '. $server_id . '.midpenmedia.org:1935/live-chans/ngrp:ch' . $ch_id . '_all/jwplayer.smil" },
+									{ file: "http://ss' . $server_id . '.midpenmedia.org:1935/live-chans/ngrp:ch' . $ch_id . '_all/playlist.m3u8" }' ;
+									
+									if (array_key_exists('url', $ch)) {
+										$now_and_next = '';
+										$rtsp_url	=	$ch["url"];
+										$stream_url	=	"{ file: '" . $ch["url"] . "' }" ;
 									}
-									</script>
-									<p class="video-details"><?php print_now_and_next(26); ?></p>
-								</li>
-								<!--<li id="pane2" class="tab-pane">
-									<div class="video-con" id='my-video27'></div>
-										<script type='text/javascript'>
+									
+									
+									$output_string	.=	'<li id="pane' . $pane_id . '" class="tab-pane ' . $is_active . '">
+										<div class="video-con" id="my-video' . $ch_id . '"></div>
+										<script type="text/javascript">
 										if( isMobile.Android() ) {
-											jQuery('#my-video27').html('<a href="rtsp://ss2.midpenmedia.org:1935/live-channels/ch27_max2700kbps">Arts, Issues &amp; Entertainment</a>');
+											jQuery("#my-video' . $ch_id . '").html(\'<a href="' . $rtsp_url . '">' . $ch_title . '</a>\');
 										} else {
-											jwplayer('my-video27').setup({
+											jwplayer("my-video' . $ch_id . '").setup({
 												playlist: [{
 													sources : [
-														{ file: 'http://ss2.midpenmedia.org:1935/live-channels/ngrp:ch27_max2700kbps/jwplayer.smil' },
-														{ file: 'http://ss2.midpenmedia.org:1935/live-channels/ngrp:ch27_max2700kbps/playlist.m3u8' }
+														' . $stream_url . '
 													],
-													// title: "Channel 27",
-													image: '<?php echo get_template_directory_uri(); ?>/images/tv-img.png'
-												}],
+													// title: "Channel ' . $ch_id . '",
+													image: "' .  get_template_directory_uri() . '/images/tv-img.png"
+												 }],
 												primary: "flash",
-												width: '505',
-												height: '265'
+												width: "505",
+												height: "265"
 											});
 										}
 										</script>
-									<p class="video-details"><?php //print_now_and_next(27); ?></p>
-								</li>-->
-								<li id="pane2" class="tab-pane">
-									<div class="video-con" id='my-video28'></div>
-									<script type='text/javascript'>
-									if( isMobile.Android() ) {
-										jQuery('#my-video28').html('<a href="rtsp://ss2.midpenmedia.org:1935/live-channels/ch28_2700kbps">Arts, Issues &amp; Entertainment</a>');
-									} else {
-										jwplayer('my-video28').setup({
-											playlist: [{
-												sources : [
-													{ file: 'http://ss2.midpenmedia.org:1935/live-channels/ngrp:ch28_max2700kbps/jwplayer.smil' },
-													{ file: 'http://ss2.midpenmedia.org:1935/live-channels/ngrp:ch28_max2700kbps/playlist.m3u8' }
-												],
-												// title: "Channel 28",
-												image: '<?php echo get_template_directory_uri(); ?>/images/tv-img.png'
-											 }],
-											primary: "flash",
-											width: '505',
-											height: '265'
-										});
-									}
-									</script>
-									<p class="video-details"><?php print_now_and_next(28); ?></p>
-								</li>
-								<li id="pane3" class="tab-pane">
-									<div class="video-con" id='my-video29'></div>
-									<script type='text/javascript'>
-									if( isMobile.Android() ) {
-										jQuery('#my-video29').html('<a href="rtsp://ss1.midpenmedia.org:1935/live-channels/ch29_200kbps">Arts, Issues &amp; Entertainment</a>');
-									} else {
-										jwplayer('my-video29').setup({
-											playlist: [{
-												sources : [
-													{ file: 'http://ss1.midpenmedia.org:1935/live-channels/ngrp:ch29_max2700kbps/jwplayer.smil' },
-													{ file: 'http://ss1.midpenmedia.org:1935/live-channels/ngrp:ch29_max2700kbps/playlist.m3u8' }
-												],
-												// title: "Channel 29",
-												image: '<?php echo get_template_directory_uri(); ?>/images/tv-img.png'
-											 }],
-											primary: "flash",
-											width: '505',
-											height: '265'
-										});
-									}
-									</script>
-									<p class="video-details"><?php print_now_and_next(29); ?></p>
-								</li>
-								<li id="pane4" class="tab-pane">
-									<div class="video-con" id='my-video30'></div>
-									<script type='text/javascript'>
-									if( isMobile.Android() ) {
-										jQuery('#my-video30').html('<a href="rtsp://ss2.midpenmedia.org:1935/live-channels/ch30_2700kbps">Arts, Issues &amp; Entertainment</a>');
-									} else {
-										jwplayer('my-video30').setup({
-											playlist: [{
-												sources : [
-													{ file: 'http://ss2.midpenmedia.org:1935/live-channels/ngrp:ch30_max2700kbps/jwplayer.smil' },
-													{ file: 'http://ss2.midpenmedia.org:1935/live-channels/ngrp:ch30_max2700kbps/playlist.m3u8' }
-												],
-												// title: "Channel 30",
-												image: '<?php echo get_template_directory_uri(); ?>/images/tv-img.png'
-											 }],
-											primary: "flash",
-											width: '505',
-											height: '265'
-										});
-									}
-									</script>
-									<p class="video-details"><?php print_now_and_next(30); ?></p>
-								</li>
-								<li id="pane5" class="tab-pane">
-									<div class="video-con" id='my-video75'></div>
-									<script type='text/javascript'>
-									if( isMobile.Android() ) {
-										jQuery('#my-video75').html('<a href="rtsp://ss2.midpenmedia.org:1935/live-channels/ch75_max2700kbps">Arts, Issues &amp; Entertainment</a>');
-									} else {
-										jwplayer('my-video75').setup({
-											playlist: [{
-												sources : [
-													{ file: 'http://ss2.midpenmedia.org:1935/live-channels/ngrp:ch75_max2700kbps/jwplayer.smil' },
-													{ file: 'http://ss2.midpenmedia.org:1935/live-channels/ngrp:ch75_max2700kbps/playlist.m3u8' }
-												],
-												// title: "Channel 27",
-												image: '<?php echo get_template_directory_uri(); ?>/images/tv-img.png'
-											}],
-											primary: "flash",
-											width: '505',
-											height: '265'
-										});
-									}
-									</script>
-									<p class="video-details"><?php print_now_and_next(75); ?></p>
-								</li>
-								<li id="pane6" class="tab-pane">
-									<div class="video-con" id='my-video27'></div>
-									<script type='text/javascript'>
-									if( isMobile.Android() ) {
-										jQuery('#my-video27').html('<a href="http://reflect.channel27.creatv.cablecast.tv/live/live.m3u8">Arts, Issues &amp; Entertainment</a>');
-									} else {
-										jwplayer('my-video27').setup({
-											playlist: [{
-												sources: [
-													{ file: 'http://reflect.channel27.creatv.cablecast.tv/live/live.m3u8' }
-
-												],
-												// title: "Channel 27",
-												image: '<?php echo get_template_directory_uri(); ?>/images/tv-img.png'
-											}],
-											primary: "flash",
-											width: '505',
-											height: '265'
-										});
-									}
-									</script>
-									<p class="video-details"><?php // print_now_and_next(27); ?></p>
-								</li>
+										<p class="video-details">' . $now_and_next . '</p>
+									</li>
+									';
+									$pane_id++;
+								endforeach;
+								
+								echo $output_string;
+								
+								?>
+								
 							</ul><!-- /.tab-content -->
 
 						</div></div>
@@ -252,23 +160,23 @@ var isMobile = {
 									<br />Channel 26</a>
 									<div class="arrow"></div>
 								</li>
-								<li><a href="#pane6" ss-no="2" channel="27" id="channel-27" data-toggle="tab">Bay Voice TV Regional Programming
+								<li><a href="#pane2" ss-no="2" channel="27" id="channel-27" data-toggle="tab">Bay Voice TV Regional Programming
 									<br />Channel 27</a>
 									<div class="arrow"></div>
 								</li>
-								<li><a href="#pane2" ss-no="2" channel="28" id="channel-28" data-toggle="tab">Youth, Education and Sports
+								<li><a href="#pane3" ss-no="2" channel="28" id="channel-28" data-toggle="tab">Youth, Education and Sports
 									<br />Channel 28</a>
 									<div class="arrow"></div>
 								</li>
-								<li><a href="#pane3" ss-no="1" channel="29" id="channel-29" data-toggle="tab">Government etc
+								<li><a href="#pane4" ss-no="1" channel="29" id="channel-29" data-toggle="tab">Government etc
 									<br />Channel 29</a>
 									<div class="arrow"></div>
 								</li>
-								<li><a href="#pane4" ss-no="2" channel="30" id="channel-30" data-toggle="tab">Arts, Issues and Lifestyles
+								<li><a href="#pane5" ss-no="2" channel="30" id="channel-30" data-toggle="tab">Arts, Issues and Lifestyles
 									<br />Channel 30</a>
 									<div class="arrow"></div>
 								</li>
-								<li><a href="#pane5" ss-no="2" channel="75" id="channel-75" data-toggle="tab">Diversity and Culture
+								<li><a href="#pane6" ss-no="2" channel="75" id="channel-75" data-toggle="tab">Diversity and Culture
 									<br />Channel 75</a>
 									<div class="arrow"></div>
 								</li>
@@ -277,24 +185,20 @@ var isMobile = {
                     <div class="clear"></div>
                     <span class="video-option"><p>Is your monthly data usage limited? Select the maximum data rate for playing this video</p> 
 									<label class="radio-inline">
-										<input type="radio" name="bitrates" id="inlineRadio1" value="2700kbps" checked="checked"> 2700 <span>kbps (MAX)</span>
+										<input type="radio" name="bitrates" id="inlineRadio1" value="all" checked="checked"> 2700 <span>kbps (MAX)</span>
 									</label>
 									<label class="radio-inline">
-										<input type="radio" name="bitrates" id="inlineRadio2" value="1600kbps"> 1600 <span>kbps</span>
+										<input type="radio" name="bitrates" id="inlineRadio2" value="max4"> 1500 <span>kbps</span>
 									</label>
 									<label class="radio-inline">
-										<input type="radio" name="bitrates" id="inlineRadio3" value="1100kbps"> 1100 <span>kbps</span>
+										<input type="radio" name="bitrates" id="inlineRadio3" value="max3"> 1000 <span>kbps</span>
 									</label>
 									<label class="radio-inline">
-										<input type="radio" name="bitrates" id="inlineRadio4" value="800kbps"> 800 <span>kbps</span>
+										<input type="radio" name="bitrates" id="inlineRadio5" value="max2"> 500 <span>kbps</span>
 									</label>
 									<label class="radio-inline">
-										<input type="radio" name="bitrates" id="inlineRadio5" value="500kbps"> 500 <span>kbps</span>
+										<input type="radio" name="bitrates" id="inlineRadio6" value="max1"> 300 <span>kbps (MIN)</span>
 									</label>
-									<label class="radio-inline">
-										<input type="radio" name="bitrates" id="inlineRadio6" value="200kbps"> 200 <span>kbps (MIN)</span>
-									</label>
-
 							</span>
 					</div>
  
@@ -312,79 +216,84 @@ var isMobile = {
 </div>
 <div class="loading-image-div"><!-- Place at bottom of page --></div>
 
-    <script type='text/javascript'>
-	jQuery(document).ready(function(){
-	    <?php
-		if( isset($_GET['channel']) ) :
-		    $selected_channel = trim($_GET['channel']) ;
-		?>
-	    var channel = <?php echo $selected_channel; ?>;
-	    jQuery( "a#channel-"+channel ).trigger("click");
-		<?php endif; ?>
-		
-		
-		jQuery(' #whats-on a').on("click", function() {
-			channel = jQuery(this).attr('channel');
-			ss_no =  jQuery(this).attr('ss-no');
-			bit_rate = jQuery( ".video-option input:radio[name=bitrates]:checked" ).val();
-			if ( channel == 27 ) {
-				jwplayer('my-video' + channel).setup({
-					playlist: [{
-						sources: [
-							{ file: 'http://reflect.channel27.creatv.cablecast.tv/live/live.m3u8' }
+<script type='text/javascript'>
+jQuery(document).ready(function(){
+	<?php
+	if( isset($_GET['channel']) ) :
+	    $selected_channel = trim($_GET['channel']) ;
+	?>
+    var channel = <?php echo $selected_channel; ?>;
+    jQuery( "a#channel-"+channel ).trigger("click");
+	<?php endif; ?>
+	
+	jQuery(' #whats-on a').on("click", function() {
+		channel = jQuery(this).attr('channel');
+		ss_no =  jQuery(this).attr('ss-no');
+		bit_rate = jQuery( ".video-option input:radio[name=bitrates]:checked" ).val();
 
-						],
-						title: "Channel ' + channel + '"
-						// image: '<?php echo get_template_directory_uri(); ?>/images/tv-img.png'
-					 }],
-					primary: "flash",
-					width: '505',
-					height: '265'
-				});
-		
-				jwplayer('my-video' + channel).play();
-				
-			} else {
-				jwplayer('my-video' + channel).setup({
-					playlist: [{
-						sources : [
-							{ file: 'http://ss' + ss_no + '.midpenmedia.org:1935/live-channels/ngrp:ch' + channel + '_max' + bit_rate + '/jwplayer.smil' },
-							{ file: 'http://ss' + ss_no + '.midpenmedia.org:1935/live-channels/ngrp:ch' + channel + '_max' + bit_rate + '/playlist.m3u8' }
-						],
-						title: "Channel ' + channel + '"
-						// image: '<?php echo get_template_directory_uri(); ?>/images/tv-img.png'
-					 }],
-					primary: "flash",
-					width: '505',
-					height: '265'
-				});
-		
-				jwplayer('my-video' + channel).load([
-						{ file: 'http://ss' + ss_no + '.midpenmedia.org:1935/live-channels/ngrp:ch' + channel + '_max' + bit_rate + '/jwplayer.smil' },
-						{ file: 'http://ss' + ss_no + '.midpenmedia.org:1935/live-channels/ngrp:ch' + channel + '_max' + bit_rate + '/playlist.m3u8' }
-				]);
-				jwplayer('my-video' + channel).play();
-			}
+		if ( channel == 27 ) {
+			jwplayer('my-video' + channel).setup({
+				playlist: [{
+					sources: [
+						{ file: 'http://reflect.channel27.creatv.cablecast.tv/live/live.m3u8' }
+
+					],
+					// title: "Channel ' + channel + '"
+					// image: '<?php echo get_template_directory_uri(); ?>/images/tv-img.png'
+				 }],
+				primary: "flash",
+				width: '505',
+				height: '265'
+			});
+	
+			jwplayer('my-video' + channel).play();
 			
-
-		});
+		} else {
+			jwplayer('my-video' + channel).setup({
+				playlist: [{
+					sources : [
+						{ file: 'http://ss' + ss_no + '.midpenmedia.org:1935/live-chans/ngrp:ch' + channel + '_' + bit_rate + '/jwplayer.smil' },
+						{ file: 'http://ss' + ss_no + '.midpenmedia.org:1935/live-chans/ngrp:ch' + channel + '_' + bit_rate + '/playlist.m3u8' }
+					],
+					title: "Channel ' + channel + '"
+					// image: '<?php echo get_template_directory_uri(); ?>/images/tv-img.png'
+				 }],
+				primary: "flash",
+				width: '505',
+				height: '265'
+			});
 	
-		jQuery(' .video-option input:radio').on("click", function() {
-			bit_rate = jQuery(this).val();
-			if( isMobile.Android() ) {
-				jQuery('#my-video' + channel).html('<a href="rtsp://ss' + ss_no + '.midpenmedia.org:1935/live-channels/ch' + channel + '_' + bit_rate + '">Arts, Issues &amp; Entertainment</a>');
-			} else {
-				jwplayer('my-video' + channel).load([
-					{ file: 'http://ss' + ss_no + '.midpenmedia.org:1935/live-channels/ngrp:ch' + channel + '_max' + bit_rate + '/jwplayer.smil' },
-					{ file: 'http://ss' + ss_no + '.midpenmedia.org:1935/live-channels/ngrp:ch' + channel + '_max' + bit_rate + '/playlist.m3u8' }
-				]);
-				jwplayer('my-video' + channel).play();
-			}
+			jwplayer('my-video' + channel).load([
+					{ file: 'http://ss' + ss_no + '.midpenmedia.org:1935/live-chans/ngrp:ch' + channel + '_' + bit_rate + '/jwplayer.smil' },
+					{ file: 'http://ss' + ss_no + '.midpenmedia.org:1935/live-chans/ngrp:ch' + channel + '_' + bit_rate + '/playlist.m3u8' }
+			]);
+			jwplayer('my-video' + channel).play();
+		}
+		
 
-		});
 	});
-	
-    </script>
+
+	jQuery(' .video-option input:radio').on("click", function() {
+		bit_rate = jQuery(this).val();
+		// alert(channel);
+		// alert(bit_rate);
+		var bitrate	=	'source';
+		if(bit_rate	==	'all') bitrate	==	'source';
+		else bitrate	=	bit_rate.replace(/[^0-9\.]/g, '');
+		if( isMobile.Android() ) {
+			jQuery('#my-video' + channel).html('<a href="rtsp://ss' + ss_no + '.midpenmedia.org:1935/live-chans/ch' + channel + '_' + bitrate + '">Arts, Issues &amp; Entertainment</a>');
+		} else {
+			jwplayer('my-video' + channel).load([
+				{ file: 'http://ss' + ss_no + '.midpenmedia.org:1935/live-chans/ngrp:ch' + channel + '_' + bit_rate + '/jwplayer.smil' },
+				{ file: 'http://ss' + ss_no + '.midpenmedia.org:1935/live-chans/ngrp:ch' + channel + '_' + bit_rate + '/playlist.m3u8' }
+			]);
+			jwplayer('my-video' + channel).play();
+		}
+
+	});
+});
+
+</script>
 <?php
 
 get_footer(); 
